c90f606c554d44728cc476230af77555b68122f7
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2016-12-13 18:14:58 +0700

    test add function

--- part-3/script.js
+++ part-3/script.js
@@ -1,5 +1,40 @@
-$(document).ready(function () {    
+var isEditingState = false;
 
+var CRUD_ACTION = {
+	getStateView: function(){
+		return isEditingState;
+	},
+	showFormEditing: function(){
+		isEditingState = true;
+		$('.form__section').show();
+	},
+	hideFormEditing: function(){
+		isEditingState = false;
+		$('.form__section').hide();
+	},
+	doSubmitFunction: function(event){
+
+		return isEditingState;
+	},
+	doAddFunction: function(){
+
+		return isEditingState;
+	},
+	doEditFunction: function(){
+
+		return isEditingState;
+	},
+	doDeleteFunction: function(){
+
+		return isEditingState;
+	}
+}
+
+$(document).ready(function () {   
+	// hide form when state false 
+	if(!CRUD_ACTION.getStateView()){
+		$('.form__section').hide();
+	}
 });
 
 $(window).on('load', function(){   
