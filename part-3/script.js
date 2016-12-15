27c9a6e0578bd64a640648d290e569d2e552dd47
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2016-12-15 13:36:15 +0700

    add some interaction

--- part-3/script.js
+++ part-3/script.js
@@ -1,34 +1,42 @@
 var CRUD_ACTION = {
+	// IN MEMORY STATE VARIABEL
 	datas: [],
 	dataEdited: {},
 	isEditingState: false,
+
 	// GETTER - SETTER
 	getDatas: function(){
 		return CRUD_ACTION.datas;
 	},
+
 	setDatas: function(datas){
 		return CRUD_ACTION.datas = datas;
 	},
+
 	getDataEdited: function(){
 		return CRUD_ACTION.dataEdited;
 	},
+
 	setDataEdited: function(dataEdited){
 		return CRUD_ACTION.dataEdited = dataEdited;
 	},
+
 	getStateView: function(){
 		return CRUD_ACTION.isEditingState;
 	},
+
 	setStateView: function(isEditingState){
 		return CRUD_ACTION.isEditingState = isEditingState;
 	},
+
 	// UI INTERACTION
 	showFormEditing: function(){
-		CRUD_ACTION.setStateView(true);
 		CRUD_ACTION.resetFormEditing();
 
 		$('.table').hide();
 		$('.form__section').show();
 	},
+
 	hideFormEditing: function(){
 		CRUD_ACTION.setStateView(false);
 		CRUD_ACTION.resetFormEditing();
@@ -36,6 +44,7 @@ var CRUD_ACTION = {
 		$('.form__section').hide();
 		$('.table').show();
 	},
+
 	resetFormEditing: function(){
 		$('#input-name').val("");
 		$('#input-email').val("");
@@ -44,7 +53,9 @@ var CRUD_ACTION = {
 
 		CRUD_ACTION.setDataEdited(null);
 	},
+
 	fillFormEditing: function(idWillShow){
+		CRUD_ACTION.setStateView(true);
 		CRUD_ACTION.showFormEditing();		
 
 		// filter by array that have match id
@@ -67,6 +78,7 @@ var CRUD_ACTION = {
 		
 		return datashow;
 	},
+
 	confimDelete: function(id, name){		
 		swal({
 		  title: "Are you sure?",
@@ -78,15 +90,21 @@ var CRUD_ACTION = {
 		  closeOnConfirm: false
 		},
 		function(){
-		  swal("Deleted!", "Your data has been deleted.", "success");
+			var datashow = jQuery.grep(CRUD_ACTION.getDatas(), function(objElement) {
+			  return objElement.id !== id;
+			});
+			CRUD_ACTION.insertListToView(datashow, true);
+		  	swal("Deleted!", "Your data has been deleted.", "success");
 		});	
 	},
-	insertListToView: function(array){
+
+	insertListToView: function(array, isUpdateStorage){
+		$('.table__body').html('');	
 		if(array){			
 			for(var i=0; i<array.length; i++){
 				var data = array[i];
 				var genderDisplay = data.gender === 1 ? "Male" : "Female";
-				var template = 	'<tr>'+
+				var template = 	'<tr data-id="'+ data.id +'">'+
 								'	<td>'+ (i+1) +'</td>'+
 								'	<td>'+ data.name +'</td>'+
 								'	<td>'+ genderDisplay +'</td>'+
@@ -100,13 +118,57 @@ var CRUD_ACTION = {
 
 				$('.table__body').append(template);				
 			}
-		}
-	},
-	removeDataFromList: function(data){
-		if(data){
+			
+			CRUD_ACTION.setDatas(array);
+			if(isUpdateStorage && isUpdateStorage === true){
+				localStorage.setItem('data-dummy', JSON.stringify(listObj));
+			}			
 
 		}
 	},
+
+	addDataToList: function(data){
+		if(data){
+			var datas = CRUD_ACTION.getDatas();
+			datas.push(data);
+			CRUD_ACTION.setDatas(datas);
+			localStorage.setItem('data-dummy', JSON.stringify(datas));
+
+			var genderDisplay = data.gender === 1 ? "Male" : "Female";
+			var template = 	'<tr data-id="'+ data.id +'">'+
+							'	<td>'+ (datas.length) +'</td>'+
+							'	<td>'+ data.name +'</td>'+
+							'	<td>'+ genderDisplay +'</td>'+
+							'	<td>'+ data.email +'</td>'+
+							'	<td>'+ data.address +'</td>'+
+							'	<td>'+
+							'		<button class="button button--radius button--blue" onclick="CRUD_ACTION.fillFormEditing(' + '\'' + data.id+ '\'' + ')">Edit</button>'+
+							'		<button class="button button--radius button--blue" onclick="CRUD_ACTION.confimDelete(' + '\'' + data.id+ '\'' + ', ' + '\'' + data.name+ '\'' + ')">Delete</button>'+
+							'	</td>'+
+							'</tr>';
+
+			$('.table__body').append(template);	
+			
+		}
+	},
+
+	updateDataInList: function(data){
+		if(data){
+			var datas = CRUD_ACTION.getDatas();
+			var dataTemp = [];
+			for(var i=0; i<datas.length; i++){
+				var dataItem = datas[i];
+				if(dataItem.id === datas.id){
+					dataItem = data;
+				}
+
+				dataTemp.push(dataItem);
+			}
+
+			CRUD_ACTION.insertListToView(dataTemp);			
+		}
+	},
+
 	// REQUEST - RESPONSE	
 	initialData: function(){
 		if(localStorage){
@@ -116,11 +178,15 @@ var CRUD_ACTION = {
 				var objParse = null;
 				try{
 					objParse = JSON.parse(dataStore);
-					CRUD_ACTION.setDatas(objParse);
-					CRUD_ACTION.insertListToView(objParse);
+					CRUD_ACTION.insertListToView(objParse, false);
 				}catch(err){}
 
 			}else{
+
+				$.get( "../json/dummy-data.json", function( data ) {
+				  console.log(data);
+				});
+
 				var listObj = [];
 				for(var i=0; i<10; i++){
 					var obj = {						
@@ -133,29 +199,45 @@ var CRUD_ACTION = {
 					listObj.push(obj);
 				}
 
-				CRUD_ACTION.setDatas(listObj);
-				CRUD_ACTION.insertListToView(listObj);
-				localStorage.setItem('data-dummy', JSON.stringify(listObj));
+				CRUD_ACTION.insertListToView(listObj, true);
 			}
 		}
 	},
+
 	doSubmitFunction: function(event){
 		event.preventDefault();
+
+		if(CRUD_ACTION.getStateView()){
+			// do update data
+			var gender = $('#input-gender-1').is(":checked") === true ? 1 : 2;
+
+			var data = CRUD_ACTION.getDataEdited();
+			data.name = $('#input-name').val();
+			data.gender = gender;
+			data.email = $('#input-email').val();
+			data.address = $('#input-address').val();
+
+			CRUD_ACTION.updateDataInList(data);
+
+		}else{
+			// do insert data
+			var gender = $('#input-gender-1').is(":checked") === true ? 1 : 2;
+
+			var data = {					
+				"id": "dummy-id-" + new Date().getTime(),
+				"name": $('#input-name').val(),
+				"gender": gender,
+				"email": $('#input-email').val(),
+				"address": $('#input-address').val()
+			}
+
+			CRUD_ACTION.addDataToList(data);
+
+
+		}
 		swal("Submitted!", "Your data has been saved.", "success");
 
 		CRUD_ACTION.hideFormEditing();
-	},
-	doAddFunction: function(){
-
-		return isEditingState;
-	},
-	doEditFunction: function(){
-
-		return isEditingState;
-	},
-	doDeleteFunction: function(){
-
-		return isEditingState;
 	}
 }
 
@@ -168,7 +250,6 @@ $(window).on('load', function(){
 	if(!CRUD_ACTION.getStateView()){
 		$('.form__section').hide();
 	}
-
 	// get initial data
 	CRUD_ACTION.initialData();
 });
